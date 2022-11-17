c8dbca2363892c882f8a9f5ea4a4069db2ff3240
Giuliano1993 <giuliano.gostinfini.93@gmail.com>
2022-11-17 11:46:35 +0100

    added short description

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 import requests
 import urllib
 import time
+import json
 from bs4 import BeautifulSoup
-import mysql.connector
+
+#import mysql.connector
+#from mysql.connector import errorcode
+#try:
+#    mydb = mysql.connector.connect(
+#        host="mysqldb",
+#        user="root",
+#        password="secret",
+#        port="4306",
+#        database="ongScraper"
+#    )
+#except mysql.connector.Error as err:
+#    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+#        print("Something is wrong with your user name or password")
+#    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+#        print("Database does not exist")
+#    else:
+#        print(err)
+#else:
+#    mydb.close()
 
 r = requests.get('https://www.amnesty.it/entra-in-azione/appelli/')
 content = r.content
@@ -13,18 +33,24 @@ for index, link in enumerate(allLinks):
     app = {
         'link': '',
         'titolo': '',
-        'img': '',
+        'imgUrl': '',
+        'image_file_name': '',
+        'short_description':''
     }
     app['link'] = link['href']
     imgTag = link.find('img')
     title = link.find('div', attrs={'class':'title-container'}).find('h4').text
-    app['title'] = title
+    app['titolo'] = title
+    sibs = link.next_siblings
+    for p in sibs:
+        app['short_description'] += p.text
     if(imgTag is not None):
-        app['img'] = imgTag['src']
+        app['imgUrl'] = imgTag['src']
         img_data = requests.get(imgTag['src']).content
-        with open('./downloadedImages/'+str(time.time())+'.jpg', 'wb') as handler:
+        filename = str(time.time())+'.jpg'
+        with open('./downloadedImages/'+filename, 'wb') as handler:
             handler.write(img_data)
-
+        app['image_file_name'] = filename
     appelli.append(app)
     prog = format(((index+1)/len(allLinks))*100,'.2f')
     print(str(prog)+'%',end='\r')
